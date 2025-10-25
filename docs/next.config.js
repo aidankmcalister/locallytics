5749f0e71f41acb68c569a59b4e28db29cf9af48
Aidan McAlister <aidankmcalister@gmail.com>
2025-10-24 21:35:53 -0400

    warning fixed

---
+++ docs/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Set the root directory to the current directory (docs)
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+module.exports = nextConfig;
