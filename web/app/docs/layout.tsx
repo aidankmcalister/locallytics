9f872c21f951d015f103cb262fe7ab4139dad14d
Aidan McAlister <aidankmcalister@gmail.com>
2025-10-25 16:44:36 -0400

    docs style updates

--- web/app/docs/layout.tsx
+++ web/app/docs/layout.tsx
@@ -1,10 +1,10 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { baseOptions } from "@/lib/layout.shared";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout tabMode="navbar" tree={source.pageTree} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
