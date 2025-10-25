43fb9e6eb5d5b34df6c7056f9cc8620545886d08
Aidan McAlister <aidankmcalister@gmail.com>
2025-10-25 13:52:56 -0400

    header items added

--- web/lib/layout.shared.tsx
+++ web/lib/layout.shared.tsx
@@ -1,5 +1,46 @@
-import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import type { BaseLayoutProps, LinkItemType } from "fumadocs-ui/layouts/shared";
 import { Origami } from "lucide-react";
+import Image from "next/image";
+import npmLogo from "@/public/npm.svg";
+import xLogo from "@/public/x.svg";
+import githubLogo from "@/public/github.svg";
+
+export const links: LinkItemType[] = [
+  {
+    text: "Docs",
+    url: "/docs",
+    active: "nested-url",
+  },
+  {
+    text: "NPM",
+    icon: <Image src={npmLogo} alt="npm" className="size-5 dark:invert" />,
+    label: "NPM",
+    type: "icon",
+    url: "https://www.npmjs.com/package/locallytics",
+    external: true,
+    secondary: true,
+  },
+  {
+    text: "X",
+    icon: <Image src={xLogo} alt="x" className="size-5 dark:invert" />,
+    label: "X",
+    type: "icon",
+    url: "https://x.com/aidankmcalister",
+    external: true,
+    secondary: true,
+  },
+  {
+    text: "GitHub",
+    icon: (
+      <Image src={githubLogo} alt="github" className="size-5 dark:invert" />
+    ),
+    label: "GitHub",
+    type: "icon",
+    url: "https://github.com/aidankmcalister/locallytics",
+    external: true,
+    secondary: true,
+  },
+];
 
 export function baseOptions(): BaseLayoutProps {
   return {
@@ -13,5 +54,6 @@ export function baseOptions(): BaseLayoutProps {
         </>
       ),
     },
+    links: links,
   };
 }
