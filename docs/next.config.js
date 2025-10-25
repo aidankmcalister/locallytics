c7a73b97864619c1d70006dda1727dc89e2b5e74
Aidan McAlister <aidankmcalister@gmail.com>
2025-10-24 21:44:18 -0400

    nextjs app added

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Set the root directory to the current directory (docs)
-  turbopack: {
-    root: __dirname,
-  },
-};
-
-module.exports = nextConfig;
